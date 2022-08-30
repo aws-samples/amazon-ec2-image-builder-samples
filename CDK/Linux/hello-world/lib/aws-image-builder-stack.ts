dbb409182bdde83894573777ecbf49123e424571
Hu Jin <hujinh@amazon.com>
2022-08-30 16:04:55 +1000

    chore: update formatting and misc cosmetics fix

--- CDK/Linux/hello-world/lib/aws-image-builder-stack.ts
+++ CDK/Linux/hello-world/lib/aws-image-builder-stack.ts
@@ -1,18 +1,18 @@
-import { Annotations, Stack, StackProps } from "aws-cdk-lib";
-import { IVpc, SecurityGroup, SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
-import { IBucket } from "aws-cdk-lib/aws-s3";
-import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
-import { ParameterTier, StringParameter } from "aws-cdk-lib/aws-ssm";
-import { Construct } from "constructs";
-import { existsSync, readdirSync, readFileSync } from "fs";
-import * as path from "path";
+import { Annotations, Stack, StackProps } from 'aws-cdk-lib';
+import { IVpc, SecurityGroup, SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { IBucket } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { ParameterTier, StringParameter } from 'aws-cdk-lib/aws-ssm';
+import { Construct } from 'constructs';
+import { existsSync, readdirSync, readFileSync } from 'fs';
+import * as path from 'path';
 import {
   AWSImageBuilderConstruct,
   ImageBuilderComponent,
   ParentImage,
   PipelineConfig,
-} from "./aws-image-builder";
-import { AWSSecureBucket } from "./aws-secure-bucket";
+} from './aws-image-builder';
+import { AWSSecureBucket } from './aws-secure-bucket';
 
 /**
  * AWS Image builder constructs stack
@@ -27,13 +27,9 @@ export class ImageBuilderStack extends Stack {
      * S3 Bucket
      * Hosts the Image builder Installation files code.
      */
-    this.imageBuilderToolsBucket = new AWSSecureBucket(
-      this,
-      "toolsBucket",
-      {}
-    ).bucket;
+    this.imageBuilderToolsBucket = new AWSSecureBucket(this, 'toolsBucket', {}).bucket;
 
-    const vpc = Vpc.fromLookup(this, "vpc", {
+    const vpc = Vpc.fromLookup(this, 'vpc', {
       isDefault: true,
     });
 
@@ -42,26 +38,25 @@ export class ImageBuilderStack extends Stack {
      * Uploads react built code to the S3 bucket and invalidates CloudFront
      */
 
-    new BucketDeployment(this, "Deploy-components", {
-      sources: [Source.asset("./image-builder-components")],
+    new BucketDeployment(this, 'Deploy-components', {
+      sources: [Source.asset('./image-builder-components')],
       destinationBucket: this.imageBuilderToolsBucket,
       memoryLimit: 3008,
       prune: false,
     });
 
     // 👇 Create a SG for a Image builder server
-    const imageBuilderSG = new SecurityGroup(this, "image-server-sg", {
+    const imageBuilderSG = new SecurityGroup(this, 'image-server-sg', {
       vpc: vpc,
       allowAllOutbound: true,
-      description: "security group for a image builder server",
+      description: 'security group for a image builder server',
     });
     // Choose the subnet Image builder server - ensure it has internet
     const imageBuilderSubnetId = vpc.selectSubnets({
       subnetType: SubnetType.PUBLIC,
     }).subnetIds[0];
 
-    const imageBuilderPipelineConfigurations =
-      this.validAndGetPipelineConfiguration();
+    const imageBuilderPipelineConfigurations = this.validAndGetPipelineConfiguration();
     if (!imageBuilderPipelineConfigurations) {
       return;
     }
@@ -86,15 +81,15 @@ export class ImageBuilderStack extends Stack {
     if (files.some((f) => !existsSync(f))) {
     }
     const componentList: ImageBuilderComponent[] = files.map((file) => ({
-      name: file.split(".")[0],
+      name: file.split('.')[0],
       data: this.getData(dir, file),
     }));
     const parentImage: ParentImage = imageBuilderPipeline.parentImage;
 
-    const amiIdLocation = new StringParameter(this, "Parameter", {
+    const amiIdLocation = new StringParameter(this, 'Parameter', {
       description: `The value of image`,
       parameterName: `ec2image_ami_${imageBuilderPipeline.name}`,
-      stringValue: "n/a",
+      stringValue: 'n/a',
       tier: ParameterTier.ADVANCED,
     });
 
@@ -120,17 +115,14 @@ export class ImageBuilderStack extends Stack {
   validAndGetPipelineConfiguration() {
     // Get pipeline details from json
     const imageBuilderPipelineConfigurations = this.node.tryGetContext(
-      "ImageBuilderPipelineConfigurations"
+      'ImageBuilderPipelineConfigurations'
     );
 
-    if (
-      imageBuilderPipelineConfigurations ||
-      imageBuilderPipelineConfigurations === ""
-    ) {
+    if (imageBuilderPipelineConfigurations || imageBuilderPipelineConfigurations === '') {
       if (Array.isArray(imageBuilderPipelineConfigurations)) {
         if (imageBuilderPipelineConfigurations.length === 0) {
           Annotations.of(this).addError(
-            "An ImageBuilder pipeline configuration list requires at least one configration, found 0"
+            'An ImageBuilder pipeline configuration list requires at least one configration, found 0'
           );
           return;
         }
@@ -147,19 +139,19 @@ export class ImageBuilderStack extends Stack {
           )
         ) {
           Annotations.of(this).addError(
-            "An ImageBuilder pipeline configuration is missing one of the following required values: name, dir, instanceProfileName, version, cfnImageRecipeName, parentImage"
+            'An ImageBuilder pipeline configuration is missing one of the following required values: name, dir, instanceProfileName, version, cfnImageRecipeName, parentImage'
           );
           return;
         }
       } else {
         Annotations.of(this).addError(
-          "The imageBuilderPipelinesConfiguration variable must be an array"
+          'The imageBuilderPipelinesConfiguration variable must be an array'
         );
         return;
       }
     } else {
       Annotations.of(this).addError(
-        "Mandaotry configuration ImageBuilderPipelineConfigurations is missing, expecting a list of pipeline configurations"
+        'Mandaotry configuration ImageBuilderPipelineConfigurations is missing, expecting a list of pipeline configurations'
       );
       return;
     }
@@ -167,13 +159,11 @@ export class ImageBuilderStack extends Stack {
   }
 
   getData = (dir: string, file: string) => {
-    const filePath = path.join(__dirname, "..", dir, file);
+    const filePath = path.join(__dirname, '..', dir, file);
     if (!existsSync(filePath)) {
-      Annotations.of(this).addError(
-        `Component file ${filePath} does not exists`
-      );
-      return "";
+      Annotations.of(this).addError(`Component file ${filePath} does not exists`);
+      return '';
     }
-    return readFileSync(path.join(__dirname, "..", dir, file)).toString();
+    return readFileSync(path.join(__dirname, '..', dir, file)).toString();
   };
 }
--- CDK/Linux/hello-world/test/aws-ec2-imagebuilder-cdk-example.test.ts
+++ CDK/Linux/hello-world/test/aws-ec2-imagebuilder-cdk-example.test.ts
@@ -1,25 +1,25 @@
-import * as cdk from "aws-cdk-lib";
-import { Template } from "aws-cdk-lib/assertions";
-import { ImageBuilderStack } from "../lib/aws-image-builder-stack";
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { ImageBuilderStack } from '../lib/aws-image-builder-stack';
 
-test("test stack can be built", () => {
+test('test stack can be built', () => {
   const context = {
-    buildCompletionNotificationEmails: ["AlejandroRosalez@example.com"],
+    buildCompletionNotificationEmails: ['AlejandroRosalez@example.com'],
     ImageBuilderPipelineConfigurations: [
       {
-        name: "sampleimg",
-        dir: "./image-builder-components",
-        instanceProfileName: "ImageBuilderInstanceProfile",
-        cfnImageRecipeName: "standalone-testrecipe02",
-        version: "1.0.6",
-        ssmParameterName: "ec2image_ami",
+        name: 'sampleimg',
+        dir: './image-builder-components',
+        instanceProfileName: 'ImageBuilderInstanceProfile',
+        cfnImageRecipeName: 'standalone-testrecipe02',
+        version: '1.0.6',
+        ssmParameterName: 'ec2image_ami',
         parentImage: {
-          "ap-southeast-2": { amiID: "ami-0b7dcd6e6fd797935" },
-          "ap-southeast-1": { amiID: "ami-055d15d9cfddf7bd3" },
-          "us-east-1": { amiID: "ami-04505e74c0741db8d" },
-          "us-east-2": { amiID: "ami-0fb653ca2d3203ac1" },
-          "us-west-1": { amiID: "ami-01f87c43e618bf8f0" },
-          "us-west-2": { amiID: "ami-0892d3c7ee96c0bf7" },
+          'ap-southeast-2': { amiID: 'ami-0b7dcd6e6fd797935' },
+          'ap-southeast-1': { amiID: 'ami-055d15d9cfddf7bd3' },
+          'us-east-1': { amiID: 'ami-04505e74c0741db8d' },
+          'us-east-2': { amiID: 'ami-0fb653ca2d3203ac1' },
+          'us-west-1': { amiID: 'ami-01f87c43e618bf8f0' },
+          'us-west-2': { amiID: 'ami-0892d3c7ee96c0bf7' },
         },
       },
     ],
@@ -29,14 +29,14 @@ test("test stack can be built", () => {
   };
   const app = new cdk.App(appProps);
 
-  const stack = new ImageBuilderStack(app, "ImageBuilderStack", {
+  const stack = new ImageBuilderStack(app, 'ImageBuilderStack', {
     env: {
-      account: process.env.CDK_DEFAULT_ACCOUNT,
-      region: process.env.CDK_DEFAULT_REGION,
+      account: '123456789012', //process.env.CDK_DEFAULT_ACCOUNT,
+      region: 'ap-southeast-2', //process.env.CDK_DEFAULT_REGION,
     },
   });
 
   const template = Template.fromStack(stack);
   console.log(template.toJSON());
-  template.resourceCountIs("AWS::ImageBuilder::ImagePipeline", 1);
+  template.resourceCountIs('AWS::ImageBuilder::ImagePipeline', 1);
 });
