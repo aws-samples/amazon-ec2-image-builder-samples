b94a3643e133823e1a47e47fcc5142948a78f703
assaf-orca <86738109+assaf-orca@users.noreply.github.com>
2022-11-09 06:37:39 +0200

    Fix Orca Security scan test component for linux token use (#44)
    Co-authored-by: Assaf Benarosh <assaf@orca.security>

--- Components/Linux/orca-security-scan-test-component-linux/scan_vm.sh
+++ Components/Linux/orca-security-scan-test-component-linux/scan_vm.sh
@@ -11,44 +11,15 @@ if (( ${#DEBUG_MODE_ON} )); then
   set -xe
 fi
 
-# login
-get_token ()
-{
-  local TOKEN=$(curl -s -S -X POST $ORCA_API_SERVER/api/user/session \
-                      -H "accept: application/json" -H "Content-Type: application/json" \
-                      -d "{ \"security_token\": \"$SECURITY_TOKEN\"}" | ./jq -r .jwt.access)
 
-  if [[ "$TOKEN" == null ]]; then
-    echo "failed to get token"
-    exit 1
-  fi
-  echo $TOKEN
-}
+SCAN_UNIQUE_ID=$(curl -s -S -X POST -H "Authorization: Token $SECURITY_TOKEN" $ORCA_API_SERVER/api/scan/asset/vm/$ACCOUNT_ID/$INSTANCE_ID?intermediate=true | ./jq -r .scan_unique_id)
 
-JWT_TOKEN=$(get_token)
-GET_TOKEN_EXIT_CODE=$?
-(( ${GET_TOKEN_EXIT_CODE} == 0 )) || (echo "$JWT_TOKEN" && exit 1)
-
-SCAN_UNIQUE_ID=$(curl -s -S -X POST -H "Authorization: Bearer $JWT_TOKEN" $ORCA_API_SERVER/api/scan/asset/vm/$ACCOUNT_ID/$INSTANCE_ID?intermediate=true | ./jq -r .scan_unique_id)
-
-echo "Scan started. Scan id: '${SCAN_UNIQUE_ID}'"
+echo "Scan started. Scan id: '${SCAN_UNIQUE_ID}' with api token"
 
 # scan status polling
 get_status ()
 {
-  local SCAN_STATUS_RESPONSE=$(curl -s -S -X GET -H "Authorization: Bearer $JWT_TOKEN" $ORCA_API_SERVER/api/scan/status/$SCAN_UNIQUE_ID)
-  local HAS_RESPONSE_CODE=$(./jq -r 'has("code")' <<< "$SCAN_STATUS_RESPONSE")
-  local RESPONSE_CODE=$(./jq -r '.code' <<< "$SCAN_STATUS_RESPONSE")
-
-  # if the token expired, get a new one
-  if [[ $HAS_RESPONSE_CODE && "$RESPONSE_CODE" == *"token_not_valid"* ]]; then
-    JWT_TOKEN=$(get_token)
-    GET_TOKEN_EXIT_CODE=$?
-    (( ${GET_TOKEN_EXIT_CODE} == 0 )) || (echo "$JWT_TOKEN" && exit 1)
-
-    SCAN_STATUS_RESPONSE=$(curl -s -S -X GET -H "Authorization: Bearer $JWT_TOKEN" $ORCA_API_SERVER/api/scan/status/$SCAN_UNIQUE_ID)
-  fi
-
+  local SCAN_STATUS_RESPONSE=$(curl -s -S -X GET -H "Authorization: Token $SECURITY_TOKEN" $ORCA_API_SERVER/api/scan/status/$SCAN_UNIQUE_ID)
   local SCAN_STATUS=$(./jq -r '.status' <<< "$SCAN_STATUS_RESPONSE")
 
   echo $SCAN_STATUS
@@ -64,6 +35,5 @@ do
 done
 
 # get asset score
-JWT_TOKEN=$(get_token)
-ASSET_SCORE=$(curl -s -S -X GET -H "Authorization: Bearer $JWT_TOKEN" $ORCA_API_SERVER/api/assets/vm_$ACCOUNT_ID\_$INSTANCE_ID | ./jq -r .state.score)
+ASSET_SCORE=$(curl -s -S -X GET -H "Authorization: Token $SECURITY_TOKEN" $ORCA_API_SERVER/api/assets/vm_$ACCOUNT_ID\_$INSTANCE_ID | ./jq -r .state.score)
 echo $ASSET_SCORE > $LOG_FILE_PATH
